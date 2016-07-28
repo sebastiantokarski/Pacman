b46586830323387ef6331024ed74564dd9eb33f9
sebastiantokarski <sebastiantokarski01@gmail.com>
2016-07-28 23:29:35 +0200

    update class

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-// Game Pacman prototype
+// Game Pacman class
 class PacmanGame {
     
+    // Objects variables
     constructor(wallArray, foodArray, bonusArray) {
         this.wallArray = wallArray;
         this.foodArray = foodArray;
@@ -8,8 +9,17 @@ class PacmanGame {
         this.table = [];
         this.rows = [];
         this.tiles = [];
+        this.tilesPos =[];
+        this.step = 0;
     };
     
+    checkTilesPosition () {
+        /*this.tiles.forEach(function () {
+            
+        });*/
+    }
+    
+    // Create map with all stuff
     createMap() {
         var table = document.createElement('table');
         
@@ -18,7 +28,17 @@ class PacmanGame {
         this.rows = this.table.getElementsByTagName('tr');
         this.tiles = this.table.getElementsByTagName('td');
         
+        this.checkTilesPosition();
+        console.log(this.tilesPos);
+       
+        
         this.addElemsToMap();
+       
+        // Add table to DOM
+        document.querySelector('body').appendChild(this.table);
+        console.log(this.rows[4].children[2]);
+        this.step=this.rows[4].children[2].clientWidth;
+        console.log(this.step);
         return this.table;
     };
     
@@ -61,14 +81,47 @@ class PacmanElem {
     
     constructor(game) {
         this.game = game;
+        this.pos=[1,0];
+        
     }
     
     addToMap() {
-        console.log(this.game.table);
-        var newPacman = document.querySelector('div');
+        var newPacman = document.createElement('div');
         newPacman.classList.add('pac-man');
-        console.log(newPacman);
-        return newPacman;
+        this.game.table.appendChild(newPacman);
+        console.log('step'+this.game.step);
+       var parent_this=this;
+         newPacman.style.top=(parent_this.pos[1] * parent_this.game.step)+'px';
+         newPacman.style.left=(parent_this.pos[0] * parent_this.game.step)+'px';
+        
+        document.addEventListener('keydown',function(e){
+                var figure=arguments;
+                console.log(figure);
+                switch(e.keyCode){
+                    case 37: 
+                            console.log(parent_this.pos[0]);
+                            console.log(parent_this.pos[1]);
+                        
+                            console.log(parent_this.game.wallArray[(parent_this.pos[1])].indexOf(parent_this.pos[0]));
+                            if(parent_this.game.wallArray[(parent_this.pos[1])].indexOf(parent_this.pos[0])<0 && parent_this.pos[0]>0){
+                                parent_this.pos[0]--;
+                                newPacman.style.left=(parent_this.pos[0] * parent_this.game.step)-23+'px';
+                            }
+                        break;
+//                    case 38: newPackman.goTop(); break;
+//                    case 39: newPackman.gotRight(); break;
+//                    case 40: newPackman.goDown(); break;
+                }
+              
+            }
+        );
+        
+       
+        
+    }
+    
+    goRight(event, elem) {
+        console.log(elem);
     }
     
     
@@ -77,18 +130,55 @@ class PacmanElem {
     }
 }
 
+class GhostElem {
+    
+    constructor(game, name) {
+        this.game = game;
+        this.name = name;
+    }
+    
+    addToMap() {
+        var wrapper = document.createElement('div');
+        var newGhost = document.createElement('div');
+        var eye = document.createElement('div');
+        var eye1 = document.createElement('div');
+        var mouth = document.createElement('div');
+        var iris = document.createElement('div');
+        var iris1 = document.createElement('div');
+        
+        wrapper.classList.add('wrapper');
+        wrapper.appendChild(newGhost);
+        newGhost.classList.add('ghost');
+        eye.classList.add('eye-left')
+        newGhost.appendChild(eye);
+        eye1.classList.add('eye-right')
+        newGhost.appendChild(eye1);
+        mouth.classList.add('mouth')
+        newGhost.appendChild(mouth);
+        iris.classList.add('iris')
+        eye.appendChild(iris);
+        iris1.classList.add('iris')
+        eye1.appendChild(iris1);
+        this.game.table.appendChild(wrapper);
+    }
+    
+}
+
 
 /*====================== DOMContentLoaded ======================*/
 document.addEventListener('DOMContentLoaded', function () {
     
     var newGame = new PacmanGame(wallArray, foodArray, bonusArray);
    
-    // Add table to DOM
-    document.querySelector('body').appendChild( newGame.createMap() );
+    newGame.createMap();
+   
     
     var pacman = new PacmanElem(newGame);
+    pacman.addToMap();
     
-    newGame.table.appendChild( pacman.addToMap() );
+    var ghost = new GhostElem(newGame);
+    ghost.addToMap();
+    
     
 });
 
@@ -132,7 +222,7 @@ var foodArray = [
     [0,1,2,3,4,5,11,12,13,14,15,16], // Row 10 
     [0,3,4,5,6,7,9,10,11,12,13,16], // Row 11
     [0,1,3,7,9,13,15,16], // Row 12
-    [1,3,4,5,6,7,8,9,10,11,12,13,15], // Row 13
+    [1,3,4,5,6,7,9,10,11,12,13,15], // Row 13
     [0,1,2,3,5,11,13,14,15,16], // Row 14
     [0,5,6,7,9,10,11,16], // Row 15
     [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16] // Row 16
